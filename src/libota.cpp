64a9052fdc3315cbf52286b974ac1394942c16f8
sfcg2 <castillo.sofia@correounivalle.edu.co>
2026-05-07 15:23:48 -0500

    cambio nuevo

--- src/libota.cpp
+++ src/libota.cpp
@@ -3,6 +3,7 @@
  *
  * Copyright 2024 Alvaro Salazar <alvaro@denkitronik.com>.
  *
+ * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
